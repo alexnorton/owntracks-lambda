11a7c92b9be182b091001b61bdcd2f83995e922e
Alex Norton <alex@alexnorton.com>
2019-01-04 22:35:55 +0000

    Send 404 response when date file doesn't exist

--- src/get.js
+++ src/get.js
@@ -5,9 +5,17 @@ const s3 = new AWS.S3();
 exports.handler = async event => {
   const key = `${event.pathParameters.date}.geojson`;
 
-  const object = await s3
-    .getObject({ Bucket: process.env.DATA_BUCKET, Key: key })
-    .promise();
+  let object;
+
+  try {
+    object = await s3
+      .getObject({ Bucket: process.env.DATA_BUCKET, Key: key })
+      .promise();
+  } catch (e) {
+    return {
+      statusCode: 404,
+    };
+  }
 
   return {
     body: object.Body.toString(),
