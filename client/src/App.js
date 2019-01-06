65cb93f0f61adacd5871e5291cb702c54796d497
Alex Norton <alexnorton@fastmail.com>
2019-01-06 22:41:19 +0000

    Basic map implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
+import Map from './Map';
+
 class App extends Component {
+  state = {};
+
+  async componentDidMount() {
+    const req = await fetch('/api/list');
+    const dates = await req.json();
+
+    const data = await Promise.all(
+      dates.map(date =>
+        fetch(`/api/get/${date}`).then(dateReq => dateReq.json())
+      )
+    );
+
+    this.setState({
+      data,
+    });
+  }
+
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div style={{ height: '100%' }}>
+        {this.state.data && <Map data={this.state.data} />}
+      </div>
+    );
   }
 }
 
